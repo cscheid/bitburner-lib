c0a4766ddfe9a59ac95e4d54d52f7ad43c1051a5
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 16:43:01 -0700

    copy-all, gh.js

---
+++ program/copy-all.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns */
+import { fetchConfig } from "/lib/gh.js";
+import { placeFile } from "/lib/fs.js";
+
+export async function main(ns) {
+  let { filesToDownload } = await fetchConfig(ns);
+  for (const file of filesToDownload) {
+    placeFile(ns, file);
+  }
+}
