36d41281b417f792ec5cc52f2dc4189a2770dcf2
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-28 11:36:28 -0700

    update

--- setup.js
+++ setup.js
@@ -3,7 +3,7 @@ import { restart } from "/lib/process.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-  await ns.exec("/program/copy-all.js");
+  await ns.exec("/program/copy-all.js", "home");
   restart(ns, "/program/solve-contracts.js", "home");
   restart(ns, "/program/rooter.js", "home");
 }
