8cb47e3ec7c901a874edbe3c7b3c19f8b4440fb1
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-28 11:30:27 -0700

    update

--- setup.js
+++ setup.js
@@ -2,4 +2,6 @@ import { command } from "/lib/ui/terminal.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
+  await ns.kill("/program/solve-contracts.js", "home");
+  await ns.run("/program/solve-contracts.js", "home");
 }
