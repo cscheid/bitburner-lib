a9882146d54044b2abbed55b6d339f093f8be7a0
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 22:17:01 -0700

    update

---
+++ program/show-hackable.js
@@ -0,0 +1,44 @@
+import { visit, totalPorts, openAllPorts } from "/lib/net.js";
+
+export function formatTable(lst)
+{
+  const maxs = lst[0].map(el => 0);
+  lst.forEach(row => {
+    row.forEach((v, i) => {
+      maxs[i] = Math.max(maxs[i], v.length);
+    });
+  });
+
+  const result = [];
+  lst.forEach(row => {
+    const strs = [];
+    row.forEach((v, i) => {
+      const pad = " ".repeat(maxs[i] - v.length);
+      strs.push(v + pad);
+    });
+    result.push(strs.join(" | "));
+  });
+  return "\n".join(result);
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+  const hosts = [];
+  let hack = ns.getHackingLevel();
+  
+  await visit(ns, (host) => {
+    if (host.requiredHackingLevel > hack)
+      return;
+    hosts.push(host);
+  });
+
+  const fmt = (v) => String(~~(v * 100) / 100);
+  
+  const out = ["name", "security level", "money"];
+  hosts.forEach(host => {
+    out.push([host.name,
+              fmt(host.securityLevel),
+              fmt(host.money)]);
+  });
+  ns.tprint(formatTable(out));
+}
