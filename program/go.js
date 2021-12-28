b69e0107197e4389228de5cd55c64786188d1c11
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 17:28:03 -0700

    update

---
+++ program/go.js
@@ -0,0 +1,19 @@
+import { visit, scanAnalyze } from "/lib/net.js";
+import { command } from "/lib/ui/terminal.js";
+
+/** @param {NS} ns */
+export async function main(ns) {
+  let target = ns.args[0];
+  let targetPath;
+  await visit(ns, (host, path) => {
+    if (host.name === target) {
+      targetPath = path.slice();
+    }
+  });
+	let net = await scanAnalyze(ns, "home", 100);
+  ns.tprint(targetPath.join(" -> "));
+  // command("home");
+  // for (const host of targetPath) {
+  //   command(`connect ${host}`);
+  // }
+}
