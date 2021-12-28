6b1f6817f07bbf0d14186f90496dd4fd125fa1ec
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 17:15:21 -0700

    correct path

--- program/copy-all.js
+++ program/copy-all.js
@@ -5,6 +5,6 @@ import { placeFile } from "/lib/fs.js";
 export async function main(ns) {
   let { filesToDownload } = await fetchConfig(ns);
   for (const file of filesToDownload) {
-    await placeFile(ns, file);
+    await placeFile(ns, '/' + file);
   }
 }
