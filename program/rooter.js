d679c93170263bee1b78391a3756502f015bb0a1
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 21:23:29 -0700

    update

---
+++ program/rooter.js
@@ -0,0 +1,14 @@
+/** @param {NS} ns */
+import { visit } from "/lib/net.js";
+
+export async function main(ns) {
+//  while (true) {
+  let hosts = [];
+  let currentLevel = ns.getHackingLevel();
+  let scan = await visit(ns, host => hosts.push(host));
+  hosts = hosts
+    .filter(host => (host.requiredHackingLevel <= currentLevel))
+    .map(host => host.name);
+  ns.tprint(JSON.stringify(hosts, null, 2));
+//  }
+}
