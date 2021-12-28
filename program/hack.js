02af20df9c124c8c3af794aeb30665568deac74c
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-28 00:15:04 -0700

    update

--- program/hack.js
+++ program/hack.js
@@ -5,11 +5,16 @@ import { bestActionAt } from "/lib/hack.js";
 export async function main(ns) {
   let name = ns.args[0];
   while (true) {
-    await visit(ns, async (node) => {
+    let target;
+    await visit(ns, (node) => {
       if (node.name === node) {
-        await bestActionAt(ns, node);
+        target = node;
       }
     });
+    if (target === undefined) {
+      throw new Error(`Couldn't find ${name}`);
+    }
+    await bestActionAt(ns, target);
     await ns.sleep(100);
   }
 }
