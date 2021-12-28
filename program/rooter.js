d908f32c482cb8efb3ee2e17b691a5f4e5dbe261
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 21:39:50 -0700

    update

--- program/rooter.js
+++ program/rooter.js
@@ -1,14 +1,16 @@
 /** @param {NS} ns */
-import { visit } from "/lib/net.js";
+import { visit, totalPorts, openAllPorts } from "/lib/net.js";
 
 export async function main(ns) {
 //  while (true) {
   let hosts = [];
   let currentLevel = ns.getHackingLevel();
   let scan = await visit(ns, host => hosts.push(host));
+  let portLevel = totalPorts(ns);
   
   hosts = hosts
     .filter(host => (host.requiredHackingLevel <= currentLevel))
+    .filter(host => (portLevel >= host.numPortsRequired))
     .map(host => host.name);
   ns.tprint(JSON.stringify(hosts, null, 2));
 //  }
