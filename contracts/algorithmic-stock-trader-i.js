748c00fa78440fba58d0b3fe6a6eb1835bc2c235
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-28 08:56:06 -0700

    update

---
+++ contracts/algorithmic-stock-trader-i.js
@@ -0,0 +1,10 @@
+// dumb way to do it, but apparently enough
+
+import { solveIt } from "/contracts/algorithmic-stock-trader.js";
+
+function solve(array)
+{
+  return solveIt(array, 1);
+}
+
+export default solve;
---
+++ contracts/algorithmic-stock-trader.js
@@ -0,0 +1,15 @@
+export function solveIt(array, count)
+{
+  if (count === 0) {
+    return 0;
+  }
+  
+  let maxProfit = 0;
+  for (let i = 0; i < array.length; ++i) {
+    for (let j = i+1; j < array.length; ++j) {
+      let profit = array[j] - array[i] + solveIt(array.slice(j+1), count - 1);
+      maxProfit = Math.max(maxProfit, profit);
+    }
+  }
+  return maxProfit;
+}
