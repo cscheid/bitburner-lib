5bc5af9d9165693b42ba9be0fce4e4636d29e734
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-27 21:42:40 -0700

    update

--- program/rooter.js
+++ program/rooter.js
@@ -1,17 +1,29 @@
 /** @param {NS} ns */
 import { visit, totalPorts, openAllPorts } from "/lib/net.js";
 
+function rootNode(ns, name) {
+  openAllPorts(ns, name);
+  ns.nuke(name);
+}
+
 export async function main(ns) {
-//  while (true) {
-  let hosts = [];
-  let currentLevel = ns.getHackingLevel();
-  let scan = await visit(ns, host => hosts.push(host));
-  let portLevel = totalPorts(ns);
+  while (true) {
+    let hosts = [];
+    let currentLevel = ns.getHackingLevel();
+    let scan = await visit(ns, host => hosts.push(host));
+    let portLevel = totalPorts(ns);
   
-  hosts = hosts
-    .filter(host => (host.requiredHackingLevel <= currentLevel))
-    .filter(host => (portLevel >= host.numPortsRequired))
-    .map(host => host.name);
-  ns.tprint(JSON.stringify(hosts, null, 2));
-//  }
+    let hostsToRoot = hosts
+        .filter(host => (host.requiredHackingLevel <= currentLevel))
+        .filter(host => (portLevel >= host.numPortsRequired))
+        .filter(host => !host.hasRootAccess)
+        .map(host => host.name);
+
+    hostsToRoot.forEach(name => {
+      ns.tprint(`Will root ${name}`);
+      rootNode(ns, name);
+    });
+
+    await ns.sleep(10000);
+  }
 }
