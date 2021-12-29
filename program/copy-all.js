450a2d792910bd9306d7a110787f31225bf9c35e
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-29 13:05:49 -0700

    update

--- program/copy-all.js
+++ program/copy-all.js
@@ -1,10 +1,15 @@
 import { fetchConfig } from "/lib/gh.js";
 import { placeFile } from "/lib/fs.js";
 
-/** @param {NS} ns */
-export async function main(ns) {
+export async function copyAll(ns)
+{
   let { filesToDownload } = await fetchConfig(ns);
   for (const file of filesToDownload) {
     await placeFile(ns, '/' + file);
   }
+}
+
+/** @param {NS} ns */
+export async function main(ns) {
+  await copyAll(ns);
 }
--- setup.js
+++ setup.js
@@ -1,9 +1,12 @@
 import { command } from "/lib/ui/terminal.js";
 import { restart } from "/lib/process.js";
+import { copyAll } from "/program/copy-all.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-  await ns.exec("/program/copy-all.js", "home");
+  await copyAll();
+  // await ns.exec("/program/copy-all.js", "home");
+  
   await restart(ns, "/program/solve-contracts.js", "home");
   await restart(ns, "/program/rooter.js", "home");
 
