9bb5c86ac867035c686e728ef0a6cbf8d8187ea7
Carlos Scheidegger <cscheid@cscheid.net>
2021-12-29 08:30:51 -0700

    update

--- program/hack-best-randomized.js
+++ program/hack-best-randomized.js
@@ -2,14 +2,16 @@ import { visit, totalPorts, openAllPorts } from "/lib/net.js";
 import { bestActionAt, bestTarget } from "/lib/hack.js";
 import { randomizedBestActionAt } from "/lib/hack.js";
 
+let log = {};
+
 /** @param {NS} ns */
 export async function main(ns) {
   let securityThresh = 1.5;
   let moneyThresh = 0.5;
   
   while (true) {
-    let currentLevel = ns.getHackingLevel();
-    let bestNode = bestTarget(ns);
+    let currentLevel = await ns.getHackingLevel();
+    let bestNode = await bestTarget(ns);
 
     if (bestNode === undefined) {
       ns.print("No nodes to hack. Sleeping...");
@@ -17,6 +19,11 @@ export async function main(ns) {
       continue;
     }
 
-    await randomizedBestActionAt(ns, bestNode);
+    await randomizedBestActionAt(ns, bestNode, [1.1, 1.5], [0.7, 0.9], log);
   }
+}
+
+export function getLog()
+{
+  return log;
 }
