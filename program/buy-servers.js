c1f26955bb7ab3dc6f7b1b68a74a93ae0a399150
Carlos Scheidegger <cscheid@cscheid.net>
2022-01-05 09:22:17 -0700

    update

--- program/buy-servers.js
+++ program/buy-servers.js
@@ -1,9 +1,13 @@
 /** @param {NS} ns **/
 import { copyAll } from "/program/copy-all.js";
 
+function fnow()
+{
+  return (new Date()).toLocaleString();
+}
+
 export async function main(ns) {
-  debugger;
-  for (let i = Number(ns.args[0]); i < Number(ns.args[1]); ++i) {
+  for (let i = Number(ns.args[0]); i <= 20; ++i) {
     let cost = 55000 * (1 << i);
     let m = 0;
     while (m < cost) {
@@ -11,8 +15,25 @@ export async function main(ns) {
       await ns.asleep(5000);
     }
     let server = ns.purchaseServer("loic", 1 << i);
+    ns.tprint(`${fnow()}: Purchased ${server} with ${1 << i} GB`);
     await copyAll(ns);
     await ns.asleep(5000);
-    ns.run("/program/loic-3.js", 1, ns.args[2], server);
+    ns.run("/program/loic-3.js", 1, ns.args[1], server);
   }
+  
+  let finalCost = 55000 * (1 << 20);
+
+  while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
+    let m = 0;
+    while (m < finalCost) {
+      m = ns.getPlayer().money;
+      await ns.asleep(5000);
+    }
+    let server = ns.purchaseServer("loic", 1 << 20);
+    ns.tprint(`${fnow()}: Purchased ${server} with ${1 << 20} GB`);
+    await copyAll(ns);
+    await ns.asleep(5000);
+    ns.run("/program/loic-3.js", 1, ns.args[1], server);
+  }
+  ns.tprint(`${fnow()}: Max servers purchased. The End.`);
 }
